36aca7245d4ff30646bde7f28f833d9af1bb8f73
namikaze-nitin <nitinsharmares@gmail.com>
2020-06-26 02:37:10 +0530

    comment update

--- src/main/april/HappyNumber.java
+++ src/main/april/HappyNumber.java
@@ -4,31 +4,45 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Write an algorithm to determine if a number n is "happy".
+ * A happy number is a number defined by the following process: 
+ * Starting with any positive integer, replace the number by the sum of the squares of its digits, 
+ * and repeat the process until the number equals 1 (where it will stay), or 
+ * it loops endlessly in a cycle which does not include 1. 
+ * 
+ * Those numbers for which this process ends in 1 are happy numbers.
+ * 
+ * Return True if n is a happy number, and False if not.
+ * @author weasel
+ *
+ */
 public class HappyNumber {
-    public boolean isHappy(int n) {
-    	
+	
+	/**
+	 * Basic Method using Set and Storing repetition of no in set
+	 * 
+	 * @param n
+	 * @return
+	 */
+    public boolean isHappyUsingSetAndSplitList(int n) {
+
     	Set<Integer> hasValue = new HashSet<>();
-    	hasValue.add(n);    	
-    	ArrayList<Integer> list = new ArrayList<>();
+    	hasValue.add(n);
         
     	while(n!=1) {
-    		System.out.println("\n\n n : " + n);
+    		int num=0;
+    		int mod=0;
         	while(n>0) {
-        		list.add(n%10);
+        		mod=n%10;
+        		num = num + mod*mod;
         		n=n/10;
         	}
-        	
-        	System.out.println("split : " + list);
-        	
-        	for(int val : list) n = n + val*val;
-        	
-        	if(hasValue.contains(n) == true)
-        		return false;
-        	else {
-        		hasValue.add(n);
-        		list.clear();
-        	}
+        	n=num;
+        	if(hasValue.contains(n) == true) return false;
+        	else hasValue.add(n);
         }
+    	hasValue.clear();
         return true;
     }
 }
